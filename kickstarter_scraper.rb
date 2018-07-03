6e16150e545207a6fdb43e6c4008b9293a64478a
Vicki Hamm <vicki.cello@gmail.com>
2018-07-03 19:44:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,8 @@ require 'pry'
     projects = {}
 
     kickstarter.css("li.project.grid_4").each do |project|
-      projects[project] = {}
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
     end
     projects
   end
