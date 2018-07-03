ac6cae302b564b1014908639909eea05c65f888d
Vicki Hamm <vicki.cello@gmail.com>
2018-07-03 19:15:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,10 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-
+  create_project_hash
 end
-create_project_hash
+
   binding.pry
-
-
-
