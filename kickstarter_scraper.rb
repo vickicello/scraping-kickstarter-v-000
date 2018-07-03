52d687fe98dcea02a7ceddb6facf05186bc0fa11
Vicki Hamm <vicki.cello@gmail.com>
2018-07-03 19:37:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ require 'pry'
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+
 
   def create_project_hash
     html = File.read('fixtures/kickstarter.html')
