f4503503df2a1827eb2a10048d6338d2d97ba355
Vicki Hamm <vicki.cello@gmail.com>
2018-07-03 19:41:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
-# project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 
   def create_project_hash
